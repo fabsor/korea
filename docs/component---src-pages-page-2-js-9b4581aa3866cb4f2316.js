c89494d2e1f205c1eab1844f736c3306215418f4
Fabian Sorqvist <fabian@sorqvist.nu>
2019-09-08 16:37:17 +0200

    ga and some fixes

--- docs/component---src-pages-page-2-js-9b4581aa3866cb4f2316.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{222:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(51),c=a(217),r=a(218);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-9b4581aa3866cb4f2316.js.map
---
+++ docs/component---src-pages-page-2-js-c4b4c5e20cb023d8dcc9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{223:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(51),c=a(218),r=a(219);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-c4b4c5e20cb023d8dcc9.js.map
